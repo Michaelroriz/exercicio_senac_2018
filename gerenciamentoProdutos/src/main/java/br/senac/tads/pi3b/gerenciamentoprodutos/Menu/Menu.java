4e153edd9d1bb973b98e3cdec0ea23883b4047b7
Michael <Michael@DESKTOP-KF7MGQQ>
2018-09-05 12:43:01 -0300

    correções e alterações no menu

--- gerenciamentoProdutos/src/main/java/br/senac/tads/pi3b/gerenciamentoprodutos/Menu/Menu.java
+++ gerenciamentoProdutos/src/main/java/br/senac/tads/pi3b/gerenciamentoprodutos/Menu/Menu.java
@@ -1,5 +1,6 @@
 package br.senac.tads.pi3b.gerenciamentoprodutos.Menu;
 
+import br.senac.tads.pi3b.gerenciamentoprodutos.DAO.ProdutoDAO;
 import java.util.Scanner;
 
 /**
@@ -10,21 +11,37 @@ public class Menu {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-  
+        ProdutoDAO p = new ProdutoDAO();
+        int opcao = -1;
+        do{ 
         System.out.println("Digite a opção desejada abaixo:");
         System.out.println("(1) Consultar produto");
         System.out.println("(2) Incluir produto");
         System.out.println("(3) Alterar produto");
         System.out.println("(4) Excluir produto");
         System.out.println("(0) Sair");
-        int opcao = entrada.nextInt();
-        do{        
+        opcao = entrada.nextInt();
+               
         
             switch (opcao) {
                 case 1:
+                    System.out.println("ID do produto a ser consultado: ");
+                    long id_concultar = entrada.nextLong();
+                    p.consultarProduto(id_concultar);
+                    break;
                 case 2:
+                    p.incluirProduto();
+                    break;
                 case 3:
+                    System.out.println("ID do produto a ser alterado: ");
+                    long id_alterar = entrada.nextLong();
+                    p.alterarDados(id_alterar);
+                    break;
                 case 4:
+                    System.out.println("ID do produto a ser excluido: ");
+                    long id_excluir = entrada.nextLong();
+                    p.excluirProduto(id_excluir);
+                    break;
                 default:
                     System.out.print("opcao inválida, digite novamente");
                     opcao = entrada.nextInt();
