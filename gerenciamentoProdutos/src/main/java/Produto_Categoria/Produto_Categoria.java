dbe94e642c2ee68e4f03ccefd87f079bc6754b59
michael.aroriz <michael.aroriz@CAS567073W1002.senacsp.edu.br>
2018-09-06 21:46:17 -0300

    adicionado a classe produto_categoria

---
+++ gerenciamentoProdutos/src/main/java/Produto_Categoria/Produto_Categoria.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Produto_Categoria;
+
+/**
+ *
+ * @author michael.aroriz
+ */
+public class Produto_Categoria {
+    private long id_produto;
+    private int id_categoria;
+
+    public long getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(long id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public int getId_categoria() {
+        return id_categoria;
+    }
+
+    public void setId_categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+    
+    
+}
